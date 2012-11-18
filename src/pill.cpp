f5ec862ca42b3a7031b4b442db444fb6a42ee4d2
Vinay Hiremath <vhiremath4@gmail.com>
2012-11-17 20:01:34 -0800

    Parsing Command-line Options

--- src/pill.cpp
+++ src/pill.cpp
@@ -23,9 +23,58 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#include <iostream>
+#include <string.h>
+
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
+	// the options:
+	// 1.) search string
+	// 2.) editor
+	// 3.) root search path
+	// 4.) role/extension array
+	// 5.) maximum results
+	const unsigned int MAX_OPTIONS = 5;
+	string options[MAX_OPTIONS];
+
+	for (int i = 0; i < MAX_OPTIONS; i++) {
+		options[i] = "";
+	}
+
+	for (int i = 1; i < argc; i++) {
+		if (!strcmp(argv[i], "--editor")) {
+			if (i != (argc - 1)) {
+				options[1] = argv[i + 1];
+				i++;
+			}
+		} else if (!strcmp(argv[i], "--path")) {
+			if (i != (argc - 1)) {
+				options[2] = argv[i + 1];
+				i++;
+			}
+		} else if (!strcmp(argv[i], "--role")) {
+			if (i != (argc - 1)) {
+				options[3] = argv[i + 1];
+				i++;
+			}
+		} else if (!strcmp(argv[i], "--max")) {
+			if (i != (argc - 1)) {
+				options[4] = argv[i + 1];
+				i++;
+			}
+		} else if (i == 1) {
+			options[0] = argv[i];
+		}
+	}
+
+	// 2.) default any options not set or invalid (empty strings)
+
+	// 3.) grab the options from external files
+
+	// 4.) execv
+
+	// 5.) while (1) { TAKE_IN_FILE_TO_OPEN }
 
 	return 0;
 }
